f0ebdc6557c35f6a87487c937d137257c6fd2f16
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-05-09 19:25:20 +0200

    54. Inyectamos MessageService en HeroService

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HEROES} from "./heroes.service";
 import { Hero} from "../model/interfaces";
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
