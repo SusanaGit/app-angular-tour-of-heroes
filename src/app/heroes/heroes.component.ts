39ff9a5e2b5017856cc6bea80da6501d3b5852fe
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-05-09 18:42:33 +0200

    49. Reemplaza el método getHeroes()

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -37,6 +37,7 @@ export class HeroesComponent {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 }
