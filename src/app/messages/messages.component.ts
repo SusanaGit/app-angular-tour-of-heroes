e117bace7ecc2c006dc4480a5c5be255e4723515
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-05-09 19:41:00 +0200

    56. Mostrar el mensaje de HeroService

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MessageService } from '../services/message.service';
 
 @Component({
   selector: 'app-messages',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './messages.component.scss'
 })
 export class MessagesComponent {
-
+  constructor(public messageService: MessageService) {}
 }
