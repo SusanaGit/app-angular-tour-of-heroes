a6db896a5329460cf595cc080dc38f6c0c130858
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-05-09 10:05:51 +0200

    30. Two-way binding

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { Hero } from '../model/interfaces';
 import {UpperCasePipe} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
   imports: [
-    UpperCasePipe
+    UpperCasePipe,
+    FormsModule
   ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss'
