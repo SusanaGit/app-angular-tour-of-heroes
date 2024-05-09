9403298b688dd5fc05f2581a273ada3893e9dcb0
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-05-09 19:56:10 +0200

    58. Añadimos MessageService a HeroesComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Hero } from '../model/interfaces';
 import {NgFor, NgIf, UpperCasePipe} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import {HeroDetailComponent} from "../hero-detail/hero-detail.component";
 import { HeroService } from '../services/hero.service';
+import {MessageService} from "../services/message.service";
 
 @Component({
   selector: 'app-heroes',
@@ -18,15 +19,13 @@ import { HeroService } from '../services/hero.service';
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss'
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit{
 
   selectedHero?: Hero;
 
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) {
-
-  }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -34,6 +33,7 @@ export class HeroesComponent {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { MessageService } from '../services/message.service';
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    NgForOf
+  ],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.scss'
 })
