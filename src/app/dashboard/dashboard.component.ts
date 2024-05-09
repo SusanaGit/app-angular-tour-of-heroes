d9a817ce677fcfd15a36a8ad171178b98ec6cf72
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-05-10 00:45:56 +0200

    64. Actualizo la vista, el controlador y los estilos de DashboardComponent

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../model/interfaces';
+import { HeroService } from '../services/hero.service';
+import {NgFor} from "@angular/common";
 
 @Component({
   selector: 'app-dashboard',
-  standalone: true,
-  imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  standalone: true,
+  imports: [
+    NgFor
+  ],
+  styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
 
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+  }
 }
