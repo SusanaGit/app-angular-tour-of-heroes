2bf4b7de7b76c0783f6f20287f5da239558443cc
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-05-10 01:04:57 +0200

    65. Añado la ruta del dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import {HeroesComponent} from "./heroes/heroes.component";
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent }
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
 ];
