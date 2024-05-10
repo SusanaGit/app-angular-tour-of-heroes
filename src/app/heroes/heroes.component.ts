cb59f537cc9c307c84efa458a66ba89ab6eee247
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-05-10 02:03:36 +0200

    70. Actualizo HeroesComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -4,7 +4,7 @@ import {NgFor, NgIf, UpperCasePipe} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import {HeroDetailComponent} from "../hero-detail/hero-detail.component";
 import { HeroService } from '../services/hero.service';
-import {MessageService} from "../services/message.service";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-heroes',
@@ -14,26 +14,20 @@ import {MessageService} from "../services/message.service";
     FormsModule,
     NgFor,
     NgIf,
-    HeroDetailComponent
+    HeroDetailComponent,
+    RouterLink
   ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss'
 })
 export class HeroesComponent implements OnInit{
 
-  selectedHero?: Hero;
-
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
