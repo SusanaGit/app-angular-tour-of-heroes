56dfd5c1013c5c0c9752503dba99bc96218103d9
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-05-10 20:53:54 +0200

    89. Añadir un nuevo hero

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -40,6 +40,14 @@ export class HeroService {
   }
 
   //////// Save methods //////////
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
   /** PUT: update the hero on the server */
   updateHero(hero: Hero): Observable<any> {
     return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
